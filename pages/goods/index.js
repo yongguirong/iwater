9d31926b41845fcd1461df05c97148d9dc0e0341
yongren <121320598@qq.com>
2023-02-19 21:32:55 +0800

    edit goods

--- pages/goods/index.js
+++ pages/goods/index.js
@@ -4,20 +4,80 @@ Page({
     /**
      * 页面的初始数据
      */
+    
     data: {
+        items:[{number:0,sale_price:5.0},{number:0,sale_price:15.0}],
+        activeKey: 0,
+        totalPrice:0,
         goods:[
-            {id:'0',title:'商品1',price:5.0,num:0,imageURL:'https://img01.yzcdn.cn/vant/ipad.jpeg'},
-            {id:'1',title:'商品2',price:15.0,num:0,imageURL:'https://img01.yzcdn.cn/vant/ipad.jpeg'}
+            {id:'0',title:'商品1',price:5.0,imageURL:'https://img01.yzcdn.cn/vant/ipad.jpeg'},
+            {id:'1',title:'商品2',price:15.0,imageURL:'https://img01.yzcdn.cn/vant/ipad.jpeg'}
         ]
         
     },
+    //标签点击，到后台获取相应分类商品列表，并设置GOODS
+    onChange(event) {
+        console.log(event.detail);
+        if(event.detail==0){
+            this.setData({
+                goods:[
+                    {id:'0',title:'商品1',price:5.0,imageURL:'https://img01.yzcdn.cn/vant/ipad.jpeg'},
+                    {id:'1',title:'商品2',price:15.0,imageURL:'https://img01.yzcdn.cn/vant/ipad.jpeg'}
+                ]
+            })
+        };
+        if(event.detail==1){
+            this.setData({
+                goods:[
+                    {id:'0',title:'商品3',price:5.0,imageURL:'https://img01.yzcdn.cn/vant/ipad.jpeg'},
+                    {id:'1',title:'商品4',price:15.0,imageURL:'https://img01.yzcdn.cn/vant/ipad.jpeg'}
+                ]
+            })
+        }
+      },
     //数量改变
-    numCut(e){
-        console.log(e.detail.value)
-    },
-    numAdd(e){
-        console.log(e.detail.value)
-    },
+    // 减去商品
+    minus(event){
+        var items = this.data.items  //获取购物车列表
+        var index = event.currentTarget.dataset.index;//获取当前点击事件的下标索引
+        var number = items[index].number; //获取购物车里面的value值
+        number--;
+        items[index].number=number;
+        console.log("number",number);
+        this.setData({
+            items: items
+        });
+        this.getTotalPrice();                               // 重新获取总价
+        },
+         // 加
+        plus(e){
+            var items = this.data.items;  //获取购物车列表
+            var index = e.currentTarget.dataset.index;//获取当前点击事件的下标索引
+            var number = items[index].number;  //获取购物车里面的value值
+            number++;
+            items[index].number = number;
+            this.setData({
+                items: items
+            });
+            this.getTotalPrice();                               // 重新获取总价
+        },
+    // 总价
+    getTotalPrice() {
+        let items = this.data.items;                  // 获取购物车列表
+        let total = 0;
+        for (let i = 0; i < items.length; i++) {         // 循环列表得到每个数据
+            if (items[i].selected) {                   // 判断选中才会计算价格
+                total+= items[i].number * items[i].sale_price;     // 所有价格加起来
+                total=total
+            }
+        }
+
+        this.setData({                                // 最后赋值到data中渲染到页面
+            items: items,
+            totalPrice: total
+        });
+        console.log(this.data.totalPrice)
+        },
     //确定 商品
     onSubmit(){
         try {
