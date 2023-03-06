60aeabe3fcf95d4dc5345235d7c17f67031037ac
yongren <121320598@qq.com>
2023-03-06 20:58:53 +0800

    add a template page

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ App({
             nickname:'yongren',
             phone:'13083389118',
             pic:'https://img.yzcdn.cn/vant/cat.jpeg',
-            financedata:{shuipiao:120,yajing:21,jifeng:10,hongbao:0.5,yongjing:0.10},
+            financedata:{shuipiao:120,yajing:212,jifeng:10,hongbao:86.5,yongjing:0.10},
             addrlist:[
                 {name: "天润1期", latitude: 33.876481, longitude: 115.779112, address: "安徽省亳州市谯城区半截楼街", province: "安徽省",city: "亳州市",district: "谯城区"},
                 {name: "谯城区人民政府", latitude: 33.87641, longitude: 115.77914, address: "安徽省亳州市谯城区薛阁办事处半截楼3号", province: "安徽省",city: "亳州市",district: "谯城区"}
--- pages/template/template.js
+++ pages/template/template.js
@@ -1,22 +1,13 @@
-// pages/ticket/index.js
-//获得APP实例
-const app = getApp();
+// pages/template/template.js
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-        tips:{tips:'shuipiao 0 yuan',navToPage:'/pages/setting/index',navText:"去哪"},
-        item:[{title:'标签1',content:'内容1'},{title:'标签2',content:'内容2'},{title:'标签3',content:'内容3'}
-        ,{title:'标签4',content:'内容4'},{title:'标签5',content:'内容5'},{title:'标签6',content:'内容6'}]
+        
     },
-    onChange(event) {
-        wx.showToast({
-          title: `切换到标签 ${event.detail.name}`,
-          icon: 'none',
-        });
-      },
+
     /**
      * 生命周期函数--监听页面加载
      */
